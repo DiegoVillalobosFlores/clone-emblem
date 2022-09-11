706760e34da9eb81163c7d65d637aafed0d5487e
Diego Villalobos <diegovilalobos397@gmail.com>
2022-09-11 20:41:34 +0200

    Add selected position functionality

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import cc from 'classcat'
+import {useState} from "react";
 
 const Home: NextPage = () => {
+  const [selectedPos, setSelectedPos] = useState<Array<number> | null>(null)
+
+  const handleGridClick = (yPos: number, xPos: number) => {
+    setSelectedPos([yPos, xPos])
+  }
+
+  const isSelectedPos = (yPos: number, xPos: number) => {
+    if(selectedPos === null) return false
+    if(yPos !== selectedPos[0]) return false;
+
+    return xPos === selectedPos[1];
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +27,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div>Tile</div>
+        {
+          selectedPos !== null &&
+            <div>
+              Selected Position: X:{selectedPos[0]}, Y:{selectedPos[1]}
+            </div>
+        }
+        {[...Array(8)].map((_, rowIndex) => (
+          <div key={`grid_${rowIndex}_0`} className={cc([styles.tile, styles.tile_container])}>
+            {[...Array(8)].map((_, colIndex) => (
+              <div
+                key={`grid_${rowIndex}_${colIndex}`}
+                className={cc([styles.tile, styles.tile_grass, isSelectedPos(rowIndex, colIndex) && styles.tile_selected])}
+                onClick={() => handleGridClick(rowIndex, colIndex)}
+              />
+            ))}
+          </div>
+        ))}
       </main>
     </div>
   )
